bb1e7c96b27e96196177ebf8ace3c685ba3a892e
Phil Pastorek <contact@hirethisdeveloper.com>
2017-03-08 20:32:06 -0500

    adding asset get route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+var utils   = require("../lib/lib_utils");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({});
+router.get('/', function (req, res, next) {
+    res.send({});
+});
+
+router.get("/:asset", function (req, res) {
+
+    var obj = {route: "/:asset", method: "GET"};
+
+    utils.send_success(res, obj);
+
+    /*require('lwip').open('image.jpg', function(err, image){
+     // check err...
+     // define a batch of manipulations and save to disk as JPEG:
+     image.batch()
+     .scale(0.75)          // scale to 75%
+     .rotate(45, 'white')  // rotate 45degs clockwise (white fill)
+     .crop(200, 200)       // crop a 200X200 square from center
+     .blur(5)              // Gaussian blur with SD=5
+     .writeFile('output.jpg', function(err){
+     // check err...
+     // done.
+     });
+
+     });*/
 });
 
 module.exports = router;
