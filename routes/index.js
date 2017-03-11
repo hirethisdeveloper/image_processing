a983c6c141cc3f0708c6220ea442b69c440f7e34
Phil Pastorek <contact@hirethisdeveloper.com>
2017-03-11 15:33:14 -0500

    sharp processing

--- routes/index.js
+++ routes/index.js
@@ -7,61 +7,29 @@ router.get('/', function (req, res, next) {
     res.send({});
 });
 
-router.get("/:asset", function (req, res) {
+router.get("/:noun/:guid", function (req, res) {
 
-    var obj    = {route: "/:asset", method: "GET"};
-    var imgurl = req.query.img;
+    // 960x720
+    // 66x66
+
+    var imgurl = "https://assets.htcsoc.net/" + req.params.noun + "/raw/" + req.params.guid + "." + ((req.query.ext) ? req.query.ext : "jpg");
     var dimRaw = req.query.dim;
 
     var darr       = dimRaw.split("x");
     var dimensions = {
-        width : darr[0],
-        height: darr[1]
+        width : parseInt(darr[0]),
+        height: parseInt(darr[1])
     };
 
-    // https://assets.htcsoc.net/hitch/raw/02adcfa9-f9e1-4f4e-96fc-0370564669ca.jpg
+    //console.log("imgurl", imgurl);
 
-    var rest = require('restler');
-    rest
-        .get(imgurl, {decoding: 'buffer'})
-        .on('complete', function (result) {
-
-            if ( result instanceof Error ) {
-                res.end("error");
-                return;
-            }
-
-            var path = require('path');
-            var ext  = path.extname(imgurl).replace(".", "");
-            var lwip = require("lwip");
-
-            lwip.open(result, ext, function (err, img) {
-
-                if ( err ) {
-                    console.log(err);
-                    res.end("error");
-                    return;
-                }
-
-                var ratio = dimensions.width / img.width();
-                img.batch()
-                    .scale(ratio)
-                    .crop(parseInt(dimensions.width), parseInt(dimensions.height))
-                    .toBuffer('jpg', function (err, buffer) {
-                        serveIt(buffer);
-                    });
-
-            });
-
-            function serveIt(buffer) {
-                var stream       = require('stream');
-                var bufferStream = new stream.PassThrough();
-                bufferStream.end(buffer);
-                bufferStream.pipe(res);
-                return;
-            }
-
-        });
+    utils.process_image(req, imgurl, dimensions).then(function (buffer) {
+        var stream       = require('stream');
+        var bufferStream = new stream.PassThrough();
+        bufferStream.end(buffer);
+        bufferStream.pipe(res);
+        return;
+    });
 
 });
 
