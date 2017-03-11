de2cb61726cbc65d8b4626d33362340c27dd9876
Phil Pastorek <contact@hirethisdeveloper.com>
2017-03-11 13:13:33 -0500

    img processing step 1

--- routes/index.js
+++ routes/index.js
@@ -9,24 +9,60 @@ router.get('/', function (req, res, next) {
 
 router.get("/:asset", function (req, res) {
 
-    var obj = {route: "/:asset", method: "GET"};
+    var obj    = {route: "/:asset", method: "GET"};
+    var imgurl = req.query.img;
+    var dimRaw = req.query.dim;
 
-    utils.send_success(res, obj);
+    var darr       = dimRaw.split("x");
+    var dimensions = {
+        width : darr[0],
+        height: darr[1]
+    };
 
-    /*require('lwip').open('image.jpg', function(err, image){
-     // check err...
-     // define a batch of manipulations and save to disk as JPEG:
-     image.batch()
-     .scale(0.75)          // scale to 75%
-     .rotate(45, 'white')  // rotate 45degs clockwise (white fill)
-     .crop(200, 200)       // crop a 200X200 square from center
-     .blur(5)              // Gaussian blur with SD=5
-     .writeFile('output.jpg', function(err){
-     // check err...
-     // done.
-     });
+    // https://assets.htcsoc.net/hitch/raw/02adcfa9-f9e1-4f4e-96fc-0370564669ca.jpg
 
-     });*/
+    var rest = require('restler');
+    rest
+        .get(imgurl, {decoding: 'buffer'})
+        .on('complete', function (result) {
+
+            if ( result instanceof Error ) {
+                res.end("error");
+                return;
+            }
+
+            var path = require('path');
+            var ext  = path.extname(imgurl).replace(".", "");
+            var lwip = require("lwip");
+
+            lwip.open(result, ext, function (err, img) {
+
+                if ( err ) {
+                    console.log(err);
+                    res.end("error");
+                    return;
+                }
+
+                var ratio = dimensions.width / img.width();
+                img.batch()
+                    .scale(ratio)
+                    .crop(parseInt(dimensions.width), parseInt(dimensions.height))
+                    .toBuffer('jpg', function (err, buffer) {
+                        serveIt(buffer);
+                    });
+
+            });
+
+            function serveIt(buffer) {
+                var stream       = require('stream');
+                var bufferStream = new stream.PassThrough();
+                bufferStream.end(buffer);
+                bufferStream.pipe(res);
+                return;
+            }
+
+        });
+
 });
 
 module.exports = router;
